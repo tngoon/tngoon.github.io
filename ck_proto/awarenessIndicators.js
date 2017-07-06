287b8bee4c749846f6224e595f7060506ec6c319
tngoon <tricia.ngoon@gmail.com>
2017-07-06 01:23:43 -0700

    updated the COGS 17 site to include links and slides and added the new CK prototype to the site

---
+++ ck_proto/awarenessIndicators.js
@@ -0,0 +1,54 @@
+function ShowHideDiv() {
+	var opendefault = document.getElementById("default-open");
+	var needspec = document.getElementById("need-specific");
+	var needsol = document.getElementById("need-solution");
+	var needprob = document.getElementById("need-problem");
+	var complete = document.getElementById("complete");
+	
+	var poscheck = document.getElementById("poscheck");
+	var speccheck = document.getElementById("speccheck");
+	var probcheck = document.getElementById("probcheck");
+	var solcheck = document.getElementById("solcheck");
+
+	var submit = document.getElementById("submit-comment");
+
+	if(probcheck.checked && !solcheck.checked) {
+		complete.style.display = "none";
+		opendefault.style.display = "none";
+		needsol.style.display = "block";
+		submit.style.backgroundColor = "#F0E68C";
+	} else if(probcheck.checked && solcheck.checked) {
+		opendefault.style.display = "none";
+		needprob.style.display = "none";
+		needsol.style.display = "none";
+		complete.style.display = "block";
+		submit.style.backgroundColor = "#90EE90"
+	} else if(solcheck.checked && !probcheck.checked) {
+		opendefault.style.display = "none";
+		needsol.style.display = "none";
+		complete.style.display = "none"
+		needprob.style.display = "block";
+		submit.style.backgroundColor = "#F0E68C";
+	} else {
+		needspec.style.display = "none";
+		needprob.style.display = "none";
+		needsol.style.display = "none";
+		complete.style.display = "none";
+		opendefault.style.display = "block";
+	}
+
+	if(poscheck.checked && !speccheck.checked) {
+		opendefault.style.display = "none";
+		complete.style.display = "none";	
+		needspec.style.display = "block";
+		submit.style.backgroundColor = "#F0E68C";
+	} else if(poscheck.checked && speccheck.checked) {
+		opendefault.style.display = "none";
+		needspec.style.display = "none";
+		complete.style.display = "block";
+		submit.style.backgroundColor = "#90EE90"
+	} else if(speccheck.checked){
+		needspec.style.display = "none";
+	} 
+
+}
