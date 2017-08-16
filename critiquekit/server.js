0f726fc7574313007274e85414ea5d97e08b21b1
tngoon <tricia.ngoon@gmail.com>
2017-08-16 11:46:36 -0700

    added node modules and got it to run on local server

--- critiquekit/server.js
+++ critiquekit/server.js
@@ -1,5 +1,11 @@
-//server.js
+var http=require('http');
+var express=require('express');
+var jsonfile=require('jsonfile');
+const PORT = process.env.PORT || 8080;
+const path = require('path');
+const INDEX = path.join(__dirname, '/public');
+const SCRIPTS = path.join(__dirname, '/js');
 
-var jsonfile = require('jsonfile')
-
-
+const server = express()
+		.use(express.static(__dirname + '/public'))
+		.listen(PORT, () => console.log('Listening on ${PORT}'));
