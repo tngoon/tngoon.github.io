85dd943af341691a66b0c6aeee9b278c9d48e994
tngoon <tricia.ngoon@gmail.com>
2017-08-31 12:21:59 -0700

    changed website description

--- critiquekit/server.js
+++ critiquekit/server.js
@@ -1,10 +1,12 @@
 var http=require('http');
 var express=require('express');
 var jsonfile=require('jsonfile');
+var path = require('path');
 const PORT = process.env.PORT || 8080;
 const path = require('path');
-const INDEX = path.join(__dirname, '/public');
 
+const INDEX = path.join(__dirname, '/public');
+const CONTROL = path.join(__dirname)
 const server = express()
 		.use(express.static(__dirname + '/public'))
 		.listen(PORT, () => console.log('Listening on ${PORT}'));
@@ -19,5 +21,3 @@ function updateJSON(file, obj) {
 		console.error(err);
 	});
 }
-
-// updateJSON(comments.json)
