fa8d7c6d4a5d2f3c426aa340ac4eb6b074d312be
tngoon <tricia.ngoon@gmail.com>
2018-08-03 16:08:04 -0700

    GTag manager attempt

--- js/main.js
+++ js/main.js
@@ -3,16 +3,12 @@
 $(document).ready(function() {
 	console.log("main.js connected");
 })
-$("#resume").click(resumeClick);
+$("#about").click(resumeClick);
 
 function resumeClick(e) {
    e.preventDefault();
    console.log("this worked");
    gtag('create','UA-123326761-1','auto');
-   gtag('event', 'click', {
-   	'event_category': 'outbound',
-   	'event_label': url,
-   	'transport_type': 'beacon',
-   	'event_callback': function(){document.location=href;}
+   gtag('send','event','link','click');
    });
  }
